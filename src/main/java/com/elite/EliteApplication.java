45570a3854c39f86fb23e86af3b9097e285d1273
himanshu-kumar-kune <himanshu.kumar@kune.consulting>
2024-08-14 22:59:19 +0530

    FIRST COMMIT

--- src/main/java/com/elite/EliteApplication.java
+++ src/main/java/com/elite/EliteApplication.java
@@ -5,15 +5,29 @@ import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import io.swagger.v3.oas.annotations.info.Info;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @OpenAPIDefinition(
-		info = @Info(title = "Elite Service", description = "Elite Service", version = "0.1"))
+        info = @Info(title = "Elite Service", description = "Elite Service", version = "0.1"))
 @ApiSecuritySchemes
 @SpringBootApplication
 public class EliteApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(EliteApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(EliteApplication.class, args);
+    }
+
+    @Bean
+    public WebMvcConfigurer configure() {
+        return new WebMvcConfigurer() {
+
+            @Override
+            public void addCorsMappings(CorsRegistry reg) {
+                reg.addMapping("/**").allowedOrigins("*");
+            }
+        };
+    }
 
 }
